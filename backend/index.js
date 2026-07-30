7b54a9a987739baa851cbcacaaa263d409db5d2c
Samarth Srivastava <samarth.s@saksoft.co.in>
2026-07-30 17:01:01 +0530

    initialised frontend structure

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,24 @@
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-    res.send("Backend Running Successfully");
+app.get("/api/test", (req, res) => {
+  res.json({
+    "appName": "FinPulse",
+    "version": "1.0.0",
+    "status" : "running"
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
