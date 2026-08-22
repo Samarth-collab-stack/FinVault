7fda8f02bf3b4cf6e04733fed658f63caa3dbe1d
Samarth Srivastava <srivastavasamarth2806@gmail.com>
2026-08-22 17:06:23 +0530

    worked on insights

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ function Dashboard() {
   const [categoryFilter, setCategoryFilter] = useState("all");
   const [editingId, setEditingId] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error,setError] = useState(null);
+  const [error, setError] = useState(null);
   const recentTransactions = [...transactions].sort((a, b) => new Date(b.date) - new Date(a.date)).slice(0, 5);
   const [editForm, setEditForm] = useState({
     description: "",
@@ -21,6 +21,44 @@ function Dashboard() {
   const selectedTransaction = transactions.find(
     (transaction) => transaction._id === editingId
   );
+
+  const totalIncome = transactions
+    .filter((transaction) => transaction.type === "credit")
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+  const totalExpense = transactions
+    .filter((transaction) => transaction.type === "debit")
+    .reduce((total, transaction) => total + transaction.amount, 0);
+
+  const balance = totalIncome - totalExpense;
+  const financialStatus = totalIncome > totalExpense ? "healthy" : totalIncome === totalExpense ? "neutral" : "debt";
+  const expenseRatio = totalIncome != 0 ? totalExpense / totalIncome * 100 : 0;
+  const spendingLevel = expenseRatio <= 50 ? "healthy" : expenseRatio >= 80 ? "high" : "moderate";
+  const categoryExpenses = transactions
+    .filter((transaction) => transaction.type === "debit")
+    .reduce((acc, transaction) => {
+      acc[transaction.category]
+        ? (acc[transaction.category] += transaction.amount)
+        : (acc[transaction.category] = transaction.amount);
+      return acc;
+    }, {});
+  const categoryPercentages = Object.entries(categoryExpenses).map(
+    ([category, amount]) => {
+      return {
+        category: category,
+        percentage: (amount / totalExpense) * 100
+      }
+    }
+  );
+  const highestExpenseCategory = categoryPercentages.length > 0
+    ? categoryPercentages.reduce(
+      (max, current) => (max.percentage > current.percentage) ? max : current
+    )
+    : null;
+  const spendingInsightAbove50 =
+    highestExpenseCategory &&
+    highestExpenseCategory.percentage > 50;
+  const spendingInsight = spendingLevel === "high" ? "highSpendingLevel" : spendingLevel === "healthy" ? "lowSpendingLevel" : "moderateSpendingLevel";
   useEffect(() => {
     if (selectedTransaction) {
       setEditForm({
@@ -120,21 +158,13 @@ function Dashboard() {
     localStorage.removeItem("token");
     navigate("/");
   };
-  const totalIncome = transactions
-    .filter((transaction) => transaction.type === "credit")
-    .reduce((total, transaction) => total + transaction.amount, 0);
-
-  const totalExpense = transactions
-    .filter((transaction) => transaction.type === "debit")
-    .reduce((total, transaction) => total + transaction.amount, 0);
-  const balance = totalIncome - totalExpense;
   return (
     <div className="p-10">
       {loading ? (
         <p>Loading Transactions...</p>
       ) : error ? (
         <p>{error}</p>
-      ):(
+      ) : (
         <>
           {transactions.length === 0 ? (
             <div>
@@ -160,7 +190,37 @@ function Dashboard() {
                   <p className="text-2xl font-bold">₹{totalIncome}</p>
                   <p>Money Coming In</p>
                 </div>
-
+                <div className="mt-8">
+                  <h2 className="text-xl font-bold">Spending Insight</h2>
+                  {highestExpenseCategory ? (
+                    <p>
+                      Your highest expense category is{" "}
+                      <strong>{highestExpenseCategory.category}</strong>{" "}
+                      at{" "}
+                      <strong>{highestExpenseCategory.percentage.toFixed(1)}%</strong>{" "}
+                      of your total spending.
+                    </p>
+                  ):(
+                    <p>No expense data available yet</p>
+                  )}
+                </div>
+                <div>
+                  {spendingInsightAbove50
+                    ? (<p>Your {highestExpenseCategory.category} expenses are very high.
+                      Consider reducing your spending in this category.
+                    </p>)
+                    : (<p>Your spending is distributed across multiple categories.</p>)
+                  }
+                </div>
+                <div className="mt-8">
+                  <h2 className="text-x1 font-bold">Expense Breakdown</h2>
+                  {Object.entries(categoryExpenses).map(([category, amount]) => (
+                    <div key={category}>
+                      <span>{category}</span>
+                      <span>₹{amount}</span>
+                    </div>
+                  ))}
+                </div>
                 <div className="p-6 bg-red-100 rounded-lg">
                   <h2 className="text-lg font-semibold">Total Expense</h2>
                   <p className="text-2xl font-bold">₹{totalExpense}</p>
@@ -171,6 +231,43 @@ function Dashboard() {
                   <h2 className="text-lg font-semibold">Balance</h2>
                   <p className="text-2xl font-bold">₹{balance}</p>
                   <p>Current Net Balance</p>
+                </div>
+                <div className="p-6 bg-green-100 rounded-lg">
+                  <h2 className="text-lg font-semibold">Financial Health</h2>
+                  {financialStatus === "healthy" ? (
+                    <div>
+                      <p>You are Spending within your Income</p>
+                    </div>
+                  ) : financialStatus === "debt" ? (
+                    <div>
+                      <p>Your expenses are Higher than your income</p>
+                      <p>You are in a current debt of {totalExpense - totalIncome}</p>
+                    </div>
+                  ) : (
+                    <p>Your income and expenses are currently balanced</p>
+                  )}
+                  {expenseRatio !== 0 ?
+                    (<p>You are spending {expenseRatio.toFixed(2)}% of your Income</p>)
+                    : (
+                      <p>No income has been recorded for this period.</p>
+                    )}
+                </div>
+                <div>
+                  {spendingInsight === "highSpendingLevel" ? (
+                    <p>
+                      Your {highestExpenseCategory.category} spending is very high.
+                      Consider reducing your expenses in this category.
+                    </p>
+                  ) : spendingInsight === "moderateSpendingLevel" ? (
+                    <p>
+                      Your spending is moderate. Keep an eye on your{" "}
+                      {highestExpenseCategory.category} expenses.
+                    </p>
+                  ) : (
+                    <p>
+                      Your expenses are looking healthy
+                    </p>
+                  )}
                 </div>
               </div>
               {/* Recent Transactions */}
@@ -312,7 +409,13 @@ function Dashboard() {
                           </button>
                         </td>
                       </tr>) : (<tr key={transaction._id} className="border-b">
-                        <td className="p-3">{transaction.date}</td>
+                        <td className="p-3">
+                          {new Date(transaction.date).toLocaleDateString("en-IN", {
+                            day: "2-digit",
+                            month: "short",
+                            year: "numeric",
+                          })}
+                        </td>
                         <td className="p-3">{transaction.description}</td>
                         <td className="p-3">{transaction.category}</td>
                         <td className="p-3">{transaction.type}</td>
